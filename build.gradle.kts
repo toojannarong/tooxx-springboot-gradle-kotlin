1248531687f1c73fe502b9f1fa7a90ecd5cf7fb1
Jannarong  Wadthong <toojannarong@users.noreply.github.com>
2021-03-09 00:50:47 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,61 +1,63 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.3"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.30"
-    kotlin("plugin.spring") version "1.4.30"
+	id("org.springframework.boot") version "2.4.3"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.30"
+	kotlin("plugin.spring") version "1.4.30"
+	jacoco
+	id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
-group = "com.tooxx"
+group = "com.xom"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-
-buildscript {
-    repositories {
-        maven {
-            url = uri("http://nexus.na.xom.com:8081/repository/maven-central/")
-        }
-        maven {
-            url = uri("http://nexus.na.xom.com:8081/repository/maven-flcit/")
-        }
-        maven {
-            url = uri("http://nexus.na.xom.com:8081/repository/maven-hosted/")
-        }
-    }
-}
-
-
 repositories {
-    maven {
-        url = uri("http://nexus.na.xom.com:8081/repository/maven-central/")
-    }
-    maven {
-        url = uri("http://nexus.na.xom.com:8081/repository/maven-flcit/")
-    }
-    maven {
-        url = uri("http://nexus.na.xom.com:8081/repository/maven-hosted/")
-    }
+	mavenCentral()
 }
-
-
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
-    }
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test) // tests are required to run before generating the report
+	reports {
+		xml.isEnabled = false
+		csv.isEnabled = true
+		csv.destination = file("$buildDir/jacocoCsv/jacocoCsv.csv")
+		html.destination = file("$buildDir/jacocoHtml")
+	}
+}
+
+
+tasks.register<Copy>("copy") {
+	from("$rootProject/pre-commit")
+	into(".git/hooks")
+}
+tasks {
+	build {
+		dependsOn(":copy")
+	}
 }
